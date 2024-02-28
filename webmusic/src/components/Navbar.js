8d2f934f5565e2201089bbd6ad425752b65effd1
tnamIT299 <trinhthanhnam2003@gmail.com>
2024-02-28 22:46:50 +0700

    upload

---
+++ webmusic/src/components/Navbar.js
@@ -0,0 +1,158 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { MusicContext } from "../Context";
+import PinnedMusic from "./PinnedMusic";
+import LikedMusic from "./LikedMusic";
+
+const Navbar = ({ keyword, handleKeyPress, setKeyword, fetchMusicData, tracks, setTracks }) => {
+  const musicContext = useContext(MusicContext);
+  const likedMusic = musicContext.likedMusic;
+  const pinnedMusic = musicContext.pinnedMusic;
+  const setResultOffset = musicContext.setResultOffset;
+  const handleSortChange = (event) => {
+    const selectedOption = event.target.value;
+  
+    if (selectedOption === 'az') {
+      // Sắp xếp danh sách từ A-Z
+      const sortedData = [...tracks].sort((a, b) => (a.album.name).localeCompare(b.album.name,'vi'));
+      setTracks(sortedData);
+    } else if (selectedOption === 'za') {
+      // Sắp xếp danh sách từ Z-A
+      const sortedData = [...tracks].sort((a, b) => (b.album.name).localeCompare(a.album.name,'vi'));
+      setTracks(sortedData);
+    } else if (selectedOption === 'newest') {
+      // Sắp xếp danh sách theo ngày mới nhất
+      const sortedData = [...tracks].sort((a, b) => {
+        return new Date(b.album.release_date) - new Date(a.album.release_date);
+      });
+      setTracks(sortedData);
+    }
+  };
+  
+  return (
+    <>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-dark sticky-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <i className="bi bi-music-note-list mx-3"></i> v-music
+          </Link>
+          <div>
+            <button
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              className="btn btn-secondary btn-sm mx-1"
+            >
+              <i className="bi bi-pin-angle-fill"></i> {pinnedMusic.length}
+            </button>
+            <button
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#likedMusicModal"
+              className="btn btn-secondary btn-sm mx-1"
+            >
+              <i className="bi bi-heart-fill"></i> {likedMusic.length}
+            </button>
+          </div>
+
+          <div
+            className="collapse navbar-collapse d-flex justify-content-center"
+            id="navbarSupportedContent"
+          >
+            <input
+              value={keyword}
+              onChange={(event) => setKeyword(event.target.value)}
+              onKeyDown={handleKeyPress}
+              className="form-control me-2 w-75"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              onClick={() => {
+                setResultOffset(0);
+                fetchMusicData();
+              }}
+              className="btn btn-outline-success"
+            >
+              Search
+            </button>
+
+            <select
+              style={{
+                padding: '10px',
+                marginLeft:'30px',
+                backgroundColor:'lightblue',
+                borderRadius:10,
+              }}
+              onChange={handleSortChange}
+              className="custom-select">
+              <option value="">Filter</option>
+              <option value="newest">Mới phát hành</option>
+              <option value="az">Sắp xếp theo tên A-Z</option>
+              <option value="za">Sắp xếp theo tên Z-A</option>
+            </select>
+
+
+          </div>
+        </div>
+      </nav>
+
+      <div
+        className="modal fade modal-xl"
+        id="exampleModal"
+        tabIndex={1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Pinned Music
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <PinnedMusic />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="modal fade modal-xl"
+        id="likedMusicModal"
+        tabIndex={1}
+        aria-labelledby="likedMusicModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="likedMusicModalLabel">
+                Liked Music
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <LikedMusic />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
